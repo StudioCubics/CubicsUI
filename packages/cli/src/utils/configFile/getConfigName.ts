6ae38332f7b4077fc3fe8f6b430d7610d7098b69
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2025-05-07 14:21:36 +0600

    🐳 chore(CLI): Removed redundant code

--- packages/cli/src/utils/configFile/getConfigName.ts
+++ packages/cli/src/utils/configFile/getConfigName.ts
@@ -5,16 +5,4 @@ export default function getConfigName(generatedConfig: CUIConfig) {
     return configFiles[1];
   }
   return configFiles[0];
-  // // By default use the "cui.config.mjs" file
-  // let finalConfigName: ConfigFile = configFiles[0];
-  // // If the detected config is typescript then use "cui.config.ts" instead
-  // if (isProjectUsingTypescript()) {
-  //   finalConfigName = configFiles[1];
-  // }
-  // // // Set back to mjs eventhough built with typescript as nextJS doesnt use typescript in runtime
-  // // if (isProjectUsingNextJs()) {
-  // //   finalConfigName = configFiles[0];
-  // // }
-
-  // return finalConfigName;
 }
