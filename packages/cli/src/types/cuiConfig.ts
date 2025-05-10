a9b83bfc2c892b81156283ac5fa1c62739127d7b
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2025-05-10 22:08:25 +0600

    🦄 refactor(CLI): Modified cui-config.schema

--- packages/cli/src/types/cuiConfig.ts
+++ packages/cli/src/types/cuiConfig.ts
@@ -3,50 +3,42 @@
 /**
  * Comprehensive configuration schema for the CubicsUI CLI. Represents the full configuration schema that controls component, library, and codeblock generation.
  */
-export type CUIConfig = EnvSection & DatabaseSection & ModsSection;
-
-export interface EnvSection {
-  envOptions: EnvOptions;
-}
-/**
- * Options about the current project's environment.
- */
-export interface EnvOptions {
+export interface CUIConfig {
   /**
-   * Set to true if the project is using TypeScript. This will be detected when you run `cui init`.
+   * Options about the current project's environment.
    */
-  typescript: boolean;
-  /**
-   * The components style module's wildcard pattern. If the component file is in `./dir/abc.jsx` then the style for the component must be in `./dir/abc.module.css`.
-   */
-  styleModulePattern?: string;
-  /**
-   * The documentation file's wildcard pattern. If the component file is `./dir/abc.jsx` then the documentation for the component will be `./dir/abc.md`.
-   */
-  documentationPattern?: string;
-}
-export interface DatabaseSection {
-  databaseOptions: DatabaseOptions;
-}
-/**
- * Options containing information about the database.
- */
-export interface DatabaseOptions {
-  /**
-   * The library that the current project is associated with in the database. This will also be used to create a library if the library doesnt exist.
-   */
-  library: {
+  envOptions: {
     /**
-     * Name of the selected library in the database where the components can be found. If the library doesn't exist then it is created in the database.
+     * Set to true if the project is using TypeScript. This will be detected when you run `cui init`.
      */
-    name: string;
+    typescript: boolean;
     /**
-     * This is used by cui to know where the root of the library is located. `baseUrl` is taken from the nearest `tsconfig.json` or `jsconfig.json`. If none of these exist then it defaults to current working directory.
+     * The components style module's wildcard pattern. If the component file is in `./dir/abc.jsx` then the style for the component must be in `./dir/abc.module.css`.
      */
-    baseUrl: string;
+    styleModulePattern?: string;
+    /**
+     * The documentation file's wildcard pattern. If the component file is `./dir/abc.jsx` then the documentation for the component will be `./dir/abc.md`.
+     */
+    documentationPattern?: string;
   };
-}
-export interface ModsSection {
+  /**
+   * Options containing information about the database.
+   */
+  databaseOptions: {
+    /**
+     * The library that the current project is associated with in the database. This will also be used to create a library if the library doesnt exist.
+     */
+    library: {
+      /**
+       * Name of the selected library in the database where the components can be found. If the library doesn't exist then it is created in the database.
+       */
+      name: string;
+      /**
+       * This is used by cui to know where the root of the library is located. `baseUrl` is taken from the nearest `tsconfig.json` or `jsconfig.json`. If none of these exist then it defaults to current working directory.
+       */
+      baseUrl: string;
+    };
+  };
   /**
    * Optional mods array.
    */
