4675e8e4611a4d40fa09ddb57d1bd1e89e0cd1d8
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2026-09-18 22:40:46 +0600

    🐞 fix(components): Fix Logo masking issues

--- packages/components/src/Misc/Logo/Logo.tsx
+++ packages/components/src/Misc/Logo/Logo.tsx
@@ -1,34 +1,54 @@
 "use client";
-import { type ReactElement } from "react";
+import { useId, type ReactElement } from "react";
 import type { LogoProps } from "./Logo.types";
 
 export function Logo(props: LogoProps): ReactElement {
   const {
     onlyFavicon,
-    faviconViewBox,
+    faviconViewBox = " ",
     favicon,
     faviconClass,
     onlyText,
-    textViewBox,
+    textViewBox = " ",
     text,
     textClass,
     viewBox,
+    style,
     ...rest
   } = props;
+
+  const faviconMaskId = useId();
+
+  const [textX, textY, textWidth, textHeight] = textViewBox
+    .split(" ")
+    .map(Number);
+
   return (
     <svg
       viewBox={onlyFavicon ? faviconViewBox : onlyText ? textViewBox : viewBox}
       xmlns="http://www.w3.org/2000/svg"
-
+      style={{ ...style }}
       {...rest}
     >
+      <defs>
+        <mask id={faviconMaskId} maskUnits="userSpaceOnUse">
+          <rect x="-50%" y="-50%" width="200%" height="200%" fill="white" />
+          <rect
+            x={textX}
+            y={textY}
+            width={textWidth}
+            height={textHeight}
+            fill="black"
+          />
+        </mask>
+      </defs>
       {/* Favicon */}
       <g
+        mask={`url(#${faviconMaskId})`}
         className={faviconClass}
         style={{
-          transition: "all var(--transition-time) var(--transition-tf)",
+          transition: `all var(--transition-time) var(--transition-tf)`,
           scale: onlyText ? "0" : "1",
-          opacity: onlyText ? "0" : "1",
         }}
       >
         {favicon}
@@ -37,9 +57,8 @@ export function Logo(props: LogoProps): ReactElement {
       <g
         className={textClass}
         style={{
-          transition: "all var(--transition-time) var(--transition-tf)",
+          transition: `all var(--transition-time) var(--transition-tf)`,
           scale: onlyFavicon ? "0" : "1",
-          opacity: onlyFavicon ? "0" : "1",
         }}
       >
         {text}
--- packages/components/src/Misc/Logo/Logo.types.ts
+++ packages/components/src/Misc/Logo/Logo.types.ts
@@ -1,14 +1,22 @@
 import type { ComponentProps } from "react";
 
 export interface LogoProps extends ComponentProps<"svg"> {
+  /** Renders only the favicon of the favicon*/
   onlyFavicon?: boolean;
-  faviconViewBox?: ComponentProps<"svg">["viewBox"];
-  favicon?: ComponentProps<"svg">["children"];
+  /** The viewbox of the favicon*/
+  faviconViewBox: Required<ComponentProps<"svg">["viewBox"]>;
+  /** The paths or svg elements of only the favicon*/
+  favicon: Required<ComponentProps<"svg">["children"]>;
+  /** Classname that will be passed to the `<g/>` wrapping the favicon*/
   faviconClass?: ComponentProps<"g">["className"];
+  /** Renders only the text of the logo */
   onlyText?: boolean;
-  textViewBox?: ComponentProps<"svg">["viewBox"];
-  text?: ComponentProps<"svg">["children"];
+  /** The viewbox of the text*/
+  textViewBox: Required<ComponentProps<"svg">["viewBox"]>;
+  /** The paths or svg elements of only the text*/
+  text: Required<ComponentProps<"svg">["children"]>;
+  /** Classname that will be passed to the `<g/>` wrapping the text*/
   textClass?: ComponentProps<"g">["className"];
-  viewBox?: ComponentProps<"svg">["viewBox"];
+  /** The viewbox of the full svg icon */
+  viewBox: ComponentProps<"svg">["viewBox"];
 }
-
